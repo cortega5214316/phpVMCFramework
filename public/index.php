c4693b557f39940660ff41f9d0aadff07facc2c7
Carlos Ortega <almal75@aol.com>
2017-11-25 16:48:00 -0800

    Added routing data

--- public/index.php
+++ public/index.php
@@ -13,12 +13,22 @@
 	require ROOT.'/app/configs/config.php'; 
 	require COREDIR.DS.'router.class.php'; 
 	
-	//echo ROOT;
-	
+	// Creates an instance of the Router
 	$router = new Router();
 	
-	echo get_class($router);
+	// add a few routes
+	$router->add('', ['controller' => 'Home', 'action' => 'index']);
+	$router->add('posts', ['controller' => 'Post', 'action' => 'index']);
+	$router->add('posts/new', ['controller' => 'Post', 'action' => 'new']);
+	
+	// display the routing table
+	//echo ROOT;
+	echo '<pre>';
+	var_dump($router->getRoutes());
+	echo '</pre>';
 	
 	
+	
+
 
 ?>
